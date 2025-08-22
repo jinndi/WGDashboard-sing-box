14117a596c341c32df6dee6b7436c15248f9c4fb
jinndi <alncores@gmail.com>
2025-08-22 22:10:06 +0300

    fix input_server_name

--- xray-install.sh
+++ xray-install.sh
@@ -235,7 +235,8 @@ get_random_free_port() {
 }
 
 input_server_name() {
-  echomsg "Enter or select a masking domain from suggested options\n 1) github.com\n 2) microsoft.com\n 3) samsung.com\n 4) nvidia.com\n 5) amd.com" 1
+  echomsg "Enter or select a masking domain from suggested options:" 1
+  echo -e " 1) github.com\n 2) microsoft.com\n 3) samsung.com\n 4) nvidia.com\n 5) amd.com"
 
   read -rp " > " option
   until [[ "$option" =~ ^[1-5]$ || "$option" =~ ^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; do
